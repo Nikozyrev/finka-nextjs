05467038c8774c59b837bf0e90a3b17f62490f43
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-05-26 14:50:57 +0300

    refactor: improve guard

--- src/app/user/layout.tsx
+++ src/app/user/layout.tsx
@@ -1,11 +1,14 @@
 import { Suspense } from 'react';
 import ServerProfile from '../../components/auth/server-profile';
+import { AuthGuard } from '../../guards/auth-guard';
 
 export default async function UserLayout({
   children
 }: {
   children: React.ReactNode;
 }) {
+  await AuthGuard();
+
   return (
     <>
       <Suspense fallback="...">
