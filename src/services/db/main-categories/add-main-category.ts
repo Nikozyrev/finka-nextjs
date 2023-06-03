4bfd266c050a20426e1d3b85063df9c713f18dfa
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-06-03 11:43:17 +0300

    refactor: add main category interface

--- src/services/db/main-categories/add-main-category.ts
+++ src/services/db/main-categories/add-main-category.ts
@@ -1,12 +1,12 @@
-import { MainCategory } from '@prisma/client';
 import { prisma } from '../../../lib/prisma';
+import { IMainCategory } from '../../../models/main-category.model';
 
 export const addMainCategory = async ({
   userId,
   name,
   categoryType,
   cashFlowSection
-}: MainCategory) => {
+}: IMainCategory) => {
   if (!name || !categoryType || !userId) return;
 
   return prisma.mainCategory.create({
