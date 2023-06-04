3fcd2f3813157279e08d562421985ca31f4afb66
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-06-04 13:07:08 +0300

    feat: add cash flow page

---
+++ src/app/user/cashflow/page.tsx
@@ -0,0 +1,10 @@
+import { Suspense } from 'react';
+import { Title } from '@tremor/react';
+
+export default async function CashFlowPage() {
+  return (
+    <main className="p-4 md:p-10 mx-auto max-w-7xl">
+      <Title className="mb-3">Cash Flow page</Title>
+    </main>
+  );
+}
