2bad3f8c8e7bd1395f8aa112864882bdacb7785c
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-06-11 12:11:39 +0300

    feat: add loading to user

---
+++ src/app/user/loading.tsx
@@ -0,0 +1,9 @@
+import { Title } from '@tremor/react';
+
+export default async function Loading() {
+  return (
+    <Title className="p-4 md:p-10 mx-auto max-w-7xl text-center">
+      Loading...
+    </Title>
+  );
+}
