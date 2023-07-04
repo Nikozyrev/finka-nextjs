9e3030328a967d05d8cfda43d841d23588052c60
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-07-04 14:39:01 +0300

    feat: add rates history saver

--- src/app/api/rates/route.ts
+++ src/app/api/rates/route.ts
@@ -16,4 +16,20 @@ const saveTodayRates = async () => {
   }
 };
 
+const saveRatesHistory = async () => {
+  const startDate = new Date();
+  let counter = 0;
+  const dayMilliseconds = 86400000;
+
+  while (counter < 365) {
+    const date = new Date(startDate.getTime() - dayMilliseconds * counter);
+    console.log(date);
+    await saveRatesOnDate(date);
+    await new Promise((res) => setTimeout(res, 2000));
+    counter += 1;
+  }
+
+  console.log('Rates fetched!');
+};
+
 export { saveTodayRates as GET };
