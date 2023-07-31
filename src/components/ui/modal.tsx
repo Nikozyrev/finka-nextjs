3c99042dd1d47984b5c8412e46222b6e60b9d589
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-07-31 20:03:36 +0300

    fix: modal styles

--- src/components/ui/modal.tsx
+++ src/components/ui/modal.tsx
@@ -42,7 +42,7 @@ export default function AppModal({ children }: { children: ReactNode }) {
             <div className="fixed inset-0 bg-black bg-opacity-25" />
           </Transition.Child>
 
-          <div className="fixed inset-0 overflow-y-auto">
+          <div className="fixed inset-0">
             <div className="flex min-h-full items-center justify-center p-4 text-center">
               <Transition.Child
                 as={Fragment}
@@ -53,7 +53,7 @@ export default function AppModal({ children }: { children: ReactNode }) {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white  text-left align-middle shadow-xl transition-all p-2">
+                <Dialog.Panel className="w-full max-w-md transform rounded-2xl bg-white text-left align-middle shadow-xl transition-all p-2">
                   {children}
                 </Dialog.Panel>
               </Transition.Child>
