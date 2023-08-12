4b0dc354870b9b12847080ec0574688a9745436a
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-08-12 11:21:10 +0300

    feat: move account form to modal

--- src/app/user/accounts/page.tsx
+++ src/app/user/accounts/page.tsx
@@ -1,16 +1,18 @@
 import { Suspense } from 'react';
-import { Title } from '@tremor/react';
 import { CashAccountsList } from '../../../components/cash-accounts/cash-accounts-list';
 import { AddCashAccountForm } from '../../../components/cash-accounts/add-cash-account-form';
 import { getCurrencies } from '../../../services/db/currencies/get-currencies';
+import { AppModal } from '../../../components/ui/modal';
+import { AddCashAccountButton } from '../../../components/cash-accounts/add-cash-account-button';
 
 export default async function AccountsPage() {
   const currencies = await getCurrencies();
 
   return (
     <main className="p-4 md:p-10 mx-auto max-w-7xl">
-      <Title>Accounts page</Title>
-      <AddCashAccountForm currencies={currencies} />
+      <AppModal RenderButton={AddCashAccountButton}>
+        <AddCashAccountForm currencies={currencies} />
+      </AppModal>
       <Suspense fallback={'Loading...'}>
         {/* @ts-expect-error Server Component */}
         <CashAccountsList />
