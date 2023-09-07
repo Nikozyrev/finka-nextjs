495465adafe5f112de8aa064db5db7472a5a3e3e
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-09-07 20:38:41 +0300

    fix: disable dark mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,7 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     './node_modules/@tremor/**/*.{js,ts,jsx,tsx}',
   ],
+  darkMode: 'class',
   theme: {
     transparent: 'transparent',
     current: 'currentColor',
