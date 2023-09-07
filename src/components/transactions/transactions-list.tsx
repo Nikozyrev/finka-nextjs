ebd90a53fd3e14d8fc82079b800e9507a285b726
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-09-07 20:40:52 +0300

    fix: return delete transaction button

--- src/components/transactions/transactions-list.tsx
+++ src/components/transactions/transactions-list.tsx
@@ -1,5 +1,6 @@
 import { Card, List, ListItem, Title } from '@tremor/react';
 import { getTransactions } from '@/services/db/transactions/get-transactions';
+import { DeleteTransactionButton } from './delete-transaction-button';
 
 export const TransactionsList = async () => {
   const transactions = await getTransactions();
@@ -16,6 +17,7 @@ export const TransactionsList = async () => {
               <span>{category.name}</span>
               <span>{cashAccount.name}</span>
               <span>{comment}</span>
+              <DeleteTransactionButton id={id} />
             </ListItem>
           )
         )}
