4f7d76e1a5cf6549a35bc8f0dfd6d4fb26ccef12
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-09-21 22:04:41 +0300

    feat: add data cache to currencies

--- src/services/db/currencies/get-currencies.ts
+++ src/services/db/currencies/get-currencies.ts
@@ -1,7 +1,11 @@
-import { prisma } from '../../../lib/prisma';
+import { unstable_cache } from 'next/cache';
+import { prisma } from '@/lib/prisma';
 
-export const getCurrencies = async () => {
-  const data = await prisma.currency.findMany();
-
-  return data;
-};
+export const getCurrencies = unstable_cache(
+  async () => {
+    const data = await prisma.currency.findMany();
+    return data;
+  },
+  ['cache-key'],
+  { revalidate: 36000 }
+);
