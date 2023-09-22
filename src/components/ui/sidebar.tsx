646c6c796e6c9792cba221318351e6c64dfcb6d9
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-09-22 17:00:11 +0300

    feat: add bg to sidebar top on mobile

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -12,7 +12,7 @@ export const Sidebar = ({ contentTop, contentBottom }: ISidebarProps) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="border-b-2 border-b-slate-400 p-2 sm:p-0">
+    <div className="border-b-2 bg-white border-r-gray-200 p-2 sm:p-0">
       <button
         onClick={() => setIsOpen(true)}
         className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
@@ -25,7 +25,10 @@ export const Sidebar = ({ contentTop, contentBottom }: ISidebarProps) => {
         border-r-2 transition-transform -translate-x-full sm:translate-x-0
         sm:static ${isOpen ? 'transform-none' : ''}`}
       >
-        <div className="h-full px-4 py-4 flex flex-col justify-between overflow-y-auto bg-white dark:bg-gray-800">
+        <div
+          className="h-full px-4 py-4 flex flex-col justify-between overflow-y-auto bg-white
+        dark:bg-gray-800"
+        >
           <div onClick={() => setIsOpen(false)}>{contentTop}</div>
           <div>{contentBottom}</div>
         </div>
