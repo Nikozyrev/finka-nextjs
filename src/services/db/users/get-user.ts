51234c8660ae3cda3ab25822618ab90ee5c66053
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-09-24 22:06:11 +0300

    refactor: remove unused method

--- src/services/db/users/get-user.ts
+++
@@ -1,7 +0,0 @@
-import { prisma } from '@/shared/lib/prisma';
-
-export const getUserByEmail = async (email: string) => {
-  if (!email) return;
-
-  return prisma.user.findFirst({ where: { email } });
-};
