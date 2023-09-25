c4a807d708e6ad2653a0f7129b0ccfcd24c7f9aa
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-09-25 18:09:47 +0300

    feat: add get transactions count

--- src/entities/transaction/index.ts
+++ src/entities/transaction/index.ts
@@ -3,3 +3,4 @@ export * from './model/transaction.model';
 export { getTransactions } from './api/get-transactions';
 export { addTransaction } from './api/add-transaction';
 export { deleteTransaction } from './api/delete-transaction';
+export { getTransactionsCount } from './api/get-transactions-count';
