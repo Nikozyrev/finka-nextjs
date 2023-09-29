77bf6c49e1c4c387fe222566ee1f8d7443afac7e
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-09-29 15:59:34 +0300

    feat: change main page

--- src/features/navigation/ui/app-button.tsx
+++ src/features/navigation/ui/app-button.tsx
@@ -1,13 +1,12 @@
-'use client';
-
-import { Button } from '@tremor/react';
 import Link from 'next/link';
 import { ROUTES } from '@/shared/constants/routes';
 
 export const AppButton = () => {
   return (
-    <Link href={ROUTES.USER}>
-      <Button>App</Button>
+    <Link href={ROUTES.USER_ACCOUNTS}>
+      <button className="py-2 px-3 text-lg bg-blue-500 text-white rounded">
+        App
+      </button>
     </Link>
   );
 };
