245d76ab4d37198c6f6f8ab3bbc5ee6103bfbbc3
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-10-02 14:43:23 +0300

    feat: add currency select

---
+++ src/entities/currency/ui/currency-select.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { AppSelect } from '@/shared/ui/select';
+import { useCurrencies } from '../model/currency-context';
+
+export async function CurrencySelect({
+  name,
+  value,
+  onValueChange,
+  className,
+}: {
+  name?: string;
+  value?: string;
+  onValueChange?: (value: string) => void;
+  className?: string;
+}) {
+  const currencies = useCurrencies();
+
+  return (
+    <AppSelect
+      options={currencies.map(({ id, name, symbol }) => ({
+        text: `${name} (${symbol})`,
+        value: id.toString(),
+      }))}
+      placeholder="Currency"
+      name={name}
+      value={value}
+      onValueChange={onValueChange}
+      className={className}
+    />
+  );
+}
