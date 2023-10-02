c881c006b168a36ce6ab20a58cac560508cdb36f
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-10-02 13:44:47 +0300

    config: add server actions experimental support

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ const withPWA = require('next-pwa')({
 });
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
+};
 
 module.exports = withPWA(nextConfig);
