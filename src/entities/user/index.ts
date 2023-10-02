190da4f8380f9dbb09851516533bbcc5a4ad0e33
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-10-02 16:15:30 +0300

    feat: add get user id method

--- src/entities/user/index.ts
+++ src/entities/user/index.ts
@@ -1,5 +1,5 @@
 export type { IUser } from './model/user.model';
 
 export { getUserInfo } from './api/get-user-info';
-
+export { getUserId } from './api/get-user-id';
 export { UserProfile } from './ui/user-profile';
