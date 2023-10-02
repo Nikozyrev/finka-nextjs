51ed3970581a6250569c7c133c46020d670da98b
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-10-02 16:27:09 +0300

    feat: add react cache to accounts

--- src/entities/cash-account/api/get-cash-account.ts
+++ src/entities/cash-account/api/get-cash-account.ts
@@ -1,17 +1,20 @@
+import { cache } from 'react';
 import { unstable_cache } from 'next/cache';
 import { prisma } from '@/shared/lib/prisma';
 
-export const getCashAccount = unstable_cache(
-  async (userId: string, id: string) => {
-    const data = await prisma.cashAccount.findUnique({
-      select: { id: true, name: true, startBalance: true, currency: true },
-      where: { userId, id },
-    });
+export const getCashAccount = cache(
+  unstable_cache(
+    async (userId: string, id: string) => {
+      const data = await prisma.cashAccount.findUnique({
+        select: { id: true, name: true, startBalance: true, currency: true },
+        where: { userId, id },
+      });
 
-    return data
-      ? { ...data, startBalance: data.startBalance.toNumber() }
-      : null;
-  },
-  ['user_account'],
-  { tags: ['accounts'] }
+      return data
+        ? { ...data, startBalance: data.startBalance.toNumber() }
+        : null;
+    },
+    ['user_account'],
+    { tags: ['accounts'] }
+  )
 );
--- src/entities/cash-account/api/get-cash-accounts.ts
+++ src/entities/cash-account/api/get-cash-accounts.ts
@@ -1,18 +1,21 @@
+import { cache } from 'react';
 import { unstable_cache } from 'next/cache';
 import { prisma } from '@/shared/lib/prisma';
 
-export const getCashAccounts = unstable_cache(
-  async (userId: string) => {
-    const data = await prisma.cashAccount.findMany({
-      select: { id: true, name: true, startBalance: true, currency: true },
-      where: { userId },
-    });
+export const getCashAccounts = cache(
+  unstable_cache(
+    async (userId: string) => {
+      const data = await prisma.cashAccount.findMany({
+        select: { id: true, name: true, startBalance: true, currency: true },
+        where: { userId },
+      });
 
-    return data.map((account) => ({
-      ...account,
-      startBalance: account.startBalance.toNumber(),
-    }));
-  },
-  ['user_accounts'],
-  { tags: ['accounts'] }
+      return data.map((account) => ({
+        ...account,
+        startBalance: account.startBalance.toNumber(),
+      }));
+    },
+    ['user_accounts'],
+    { tags: ['accounts'] }
+  )
 );
