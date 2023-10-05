76f5effc54b883d7643c7e970da7e5cb868ce4b4
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-10-05 14:19:33 +0300

    feat: add cache for main categories

--- src/entities/main-category/index.ts
+++ src/entities/main-category/index.ts
@@ -1,4 +1,7 @@
 export * from './model/main-category.model';
 
 export { addMainCategory } from './api/add-main-category';
-export { getMainCategories } from './api/get-main-categories';
+export {
+  getMainCategories,
+  getUserMainCategories,
+} from './api/get-main-categories';
--- src/features/add-category/ui/add-categories-tabs.tsx
+++ src/features/add-category/ui/add-categories-tabs.tsx
@@ -1,10 +1,10 @@
 import { AppTabs, ITab } from '@/shared/ui/tabs';
 import { AddCategoryForm } from './add-category-form';
 import { AddMainCategoryForm } from './add-main-category-form';
-import { getMainCategories } from '@/entities/main-category';
+import { getUserMainCategories } from '@/entities/main-category';
 
 export async function AddCategoriesTabs() {
-  const mainCategories = await getMainCategories();
+  const mainCategories = await getUserMainCategories();
 
   const tabs: ITab[] = [
     {
