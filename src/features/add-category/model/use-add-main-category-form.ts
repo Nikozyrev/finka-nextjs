6823524f96077b809ecb5de547ef5277edd302cc
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-10-18 13:34:36 +0300

    refactor: add use main category form

---
+++ src/features/add-category/model/use-add-main-category-form.ts
@@ -0,0 +1,46 @@
+import { useRouter } from 'next/navigation';
+import { FormEvent } from 'react';
+import { CashFlowSection, CategoryType } from '@prisma/client';
+import { useValidatedForm } from '@/shared/hooks/form/use-validated-form';
+import { required } from '@/shared/hooks/form/validators';
+import { addMainCategory } from '../api/add-main-category';
+
+const initialState: {
+  name: string;
+  categoryType: CategoryType;
+  cashFlowSection: CashFlowSection;
+} = {
+  name: '',
+  categoryType: CategoryType.INCOME,
+  cashFlowSection: CashFlowSection.OPERATIONAL,
+};
+
+export function useAddMainCategoryForm() {
+  const Router = useRouter();
+
+  const form = useValidatedForm({
+    initialState,
+    validators: {
+      name: [required],
+      categoryType: [required],
+      cashFlowSection: [required],
+    },
+  });
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const { name, categoryType, cashFlowSection } = form.getState();
+
+    if (!name || !categoryType || !cashFlowSection) return;
+
+    await addMainCategory({ name, categoryType, cashFlowSection });
+    form.reset();
+    Router.refresh();
+  };
+
+  return {
+    handleSubmit,
+    ...form,
+  };
+}
