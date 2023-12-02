221cb2dbe7972a7df79287e724145594a7b10dc7
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-12-02 16:18:47 +0300

    refactor: navbar

--- src/features/navigation/ui/navbar.tsx
+++ src/features/navigation/ui/navbar.tsx
@@ -1,13 +1,14 @@
 'use client';
 
+import clsx from 'clsx';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { routes } from '@/shared/constants/routes';
 import { DashboardIcon } from '@/shared/ui/icons/dashboard-icon';
 
-export const NavBar = () => {
+export function NavBar() {
   const path = usePathname();
-  const currentRoute = routes.find(({ link }) => link === path);
+  const currentRoute = routes.find(({ link }) => path.startsWith(link));
 
   return (
     <ul className="space-y-2 font-medium">
@@ -15,9 +16,10 @@ export const NavBar = () => {
         <li key={link}>
           <Link
             href={link}
-            className={`flex items-center p-2 rounded-lg 
-            dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group 
-            ${link === currentRoute?.link ? 'text-blue-500' : 'text-gray-900'}`}
+            className={clsx(
+              'flex items-center p-2 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group',
+              link === currentRoute?.link ? 'text-blue-500' : 'text-gray-900'
+            )}
           >
             <DashboardIcon />
 
@@ -27,4 +29,4 @@ export const NavBar = () => {
       ))}
     </ul>
   );
-};
+}
