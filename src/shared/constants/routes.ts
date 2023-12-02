c54ac5d46e02c03ecc7a6f8a75e9a33b0ad3c5d6
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-12-02 16:18:22 +0300

    feat: add dashboard page link

--- src/shared/constants/routes.ts
+++ src/shared/constants/routes.ts
@@ -6,15 +6,17 @@ export interface IRoute {
 export enum ROUTES {
   MAIN = '/',
   USER = '/user',
+  USER_DASHBOARD = '/user/dashboard',
   USER_CASH_FLOW = '/user/cashflow',
   USER_ACCOUNTS = '/user/accounts',
   USER_TRANSACTIONS = '/user/transactions',
-  USER_CATEGORIES = '/user/categories'
+  USER_CATEGORIES = '/user/categories',
 }
 
 export const routes: IRoute[] = [
+  { text: 'Dashboard', link: ROUTES.USER_DASHBOARD },
   { text: 'Cash Flow', link: ROUTES.USER_CASH_FLOW },
   { text: 'Accounts', link: ROUTES.USER_ACCOUNTS },
   { text: 'Transactions', link: ROUTES.USER_TRANSACTIONS },
-  { text: 'Categories', link: ROUTES.USER_CATEGORIES }
+  { text: 'Categories', link: ROUTES.USER_CATEGORIES },
 ];
